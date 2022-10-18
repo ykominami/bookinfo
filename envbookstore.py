8401155497387d34d03be10ae8277690ad937caa
ykominami <ykominami@gmail.com>
2022-10-19 03:29:20 +0900

    remvoe unused files

--- envbookstore.py
+++
@@ -1,5 +0,0 @@
-from envbase import EnvBase
-
-class EnvBookstore(EnvBase):
-  def __init__(self, year, db_dir):
-    super.__init__(year, db_dir)
--- k_createdb.py
+++
@@ -1,8 +0,0 @@
-from env2 import Env2
-from kindlelist import KindleList
-#import sys
-
-Env2.init()
-kindlelist = KindleList(Env2)
-#Env2.ensure_connect()
-
